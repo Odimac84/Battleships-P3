a9d7a5feef9d87ea5f249fd913397c857b8670fc
Odimac84 <bjorn.nilsson1984@gmail.com>
2021-09-09 19:28:43 +0000

    add bugfix line 58 and 60. code went outside the board and caused indexerror. line 106 fix check for both hit and miss

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from random import randint
 
-"""
-Markups on the board to show if we hit or missed.
-"""
+
+#Markups on the board to show if we hit or missed.
+
 #Legend
 # X for placing ship and hit battleship
 # ' ' for empty plot
@@ -10,33 +10,35 @@ Markups on the board to show if we hit or missed.
 
 
 """
-Defining 2 boards, one for the player to have their guesses on and one with
-the correct solution on it. ANSWER_BOARD is hidden and used to check locations
-against only.
+Defining 2 boards, one for the player to have their guesses on and one with 
+the correct solution on it. ANSWER_BOARD is hidden and used to check locations against only 
 """
+
 
 ANSWER_BOARD = [[' '] * 9 for x in range(9)]
 PLAYER_BOARD = [[' '] * 9 for x in range(9)]
 
 """
-Converting to letters to use at the top of the board for easier overview of
+Converting to letters to use at the top of the board for easier overview of 
 the gameboard.
 """
+
+
 letters_convert = {
-    'A': 0,
-    'B': 1,
-    'C': 2,
-    'D': 3,
-    'E': 4,
-    'F': 5,
+    'A': 0, 
+    'B': 1, 
+    'C': 2, 
+    'D': 3, 
+    'E': 4, 
+    'F': 5, 
     'G': 6,
-    'H': 7,
+    'H': 7, 
     'I': 8
 }
 
-"""
-Loop to build the boards for both the PLAYER_BOARD and the ANSWER_BOARD.
-"""
+
+#Loop to build the boards for both the PLAYER_BOARD and the ANSWER_BOARD.
+
 
 
 def print_board(board):
@@ -47,22 +49,21 @@ def print_board(board):
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
-"""
-Loop to Put out the 5 battleships that we are gonna try and find.
-"""
+
+#Loop to Put out the 5 battleships that we are gonna try and find.
 
 
 def create_ships(board):
     for ship in range(5):
-        ship_row, ship_column = randint(0, 9), randint(0, 9)
-    while board[ship_row][ship_column] == 'X':
-        ship_row, ship_column = randint(0, 9), randint(0, 9)
+      ship_row, ship_column = randint(0, 8), randint(0, 8)
+      while board[ship_row][ship_column] == 'X':
+        ship_row, ship_column = randint(0, 8), randint(0, 8)
     board[ship_row][ship_column] = 'X'
-
+  
 """
-Function to add the input data on where to drop the bomb. built with loops to
-make sure correct data is entered. .upper is used to make sure that it will
-always be an uppercase letter that is provided for the code to run.
+Function to add the input data on where to drop the bomb. built with loops to 
+make sure correct data is entered. .upper is used to make sure that it will always be
+an uppercase letter that is provided for the code to run.
 """
 
 
@@ -77,9 +78,9 @@ def get_ship_location():
         column = input('Please enter a ship column A-I: ').upper()
     return int(row) - 1, letters_convert[column]
 
-"""
-Counts the hits and tells us how many hits we have.
-"""
+
+#Counts the hits and tells us how many hits we have. 
+
 
 
 def count_hit_ships(board):
@@ -91,31 +92,32 @@ def count_hit_ships(board):
     return count
 
 create_ships(ANSWER_BOARD)
-print_board(ANSWER_BOARD)
+
 turns = 25
-"""
-function to check if the input data is a hit or a miss, also checks if
-your winning. 25 turns to find the ships or you lose.
-"""
+
+#function to check if the input data is a hit or a miss, also checks if your winning. 25 turns to find the ships or you lose.
+
+
 
 while turns > 0:
     print('LETS PLAY BATTLESHIPS')
     print_board(PLAYER_BOARD)
     row, column = get_ship_location()
-    if PLAYER_BOARD[row][column] == '-' or 'X':
+    if PLAYER_BOARD[row][column] == 'X' or PLAYER_BOARD[row][column] == '*':
         print('You already guessed that')
     elif ANSWER_BOARD[row][column] == 'X':
         print(' HIT, you have sunk a ship.')
         PLAYER_BOARD[row][column] = 'X'
         turns -= 1
+        print('You have ' + str(turns) + ' turns remaining')
     else:
         print('MISS, try again')
-        PLAYER_BOARD[row][column] = '-'
+        PLAYER_BOARD[row][column] = '*'
         turns -= 1
+        print('You have ' + str(turns) + ' turns remaining')
     if count_hit_ships(PLAYER_BOARD) == 5:
         print('Congratulations, you won no more ships present')
         break
-        print('You have ' + str(turns) + ' turns remaining')
     if turns == 0:
         print('No more bombs left you got captured')
         break
