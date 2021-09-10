6bbdb4deca5f2237c0d95d02197f33ca3afd971e
Odimac84 <bjorn.nilsson1984@gmail.com>
2021-09-10 21:05:07 +0000

    add fix for counter to be able to get win message.

--- run.py
+++ run.py
@@ -1,75 +1,66 @@
 from random import randint
 
+# Markups on the board to show if we hit or missed.
 
-#Markups on the board to show if we hit or missed.
-
-#Legend
 # X for placing ship and hit battleship
 # ' ' for empty plot
-# '-' for missed shot
+# '*' for missed shot
 
 
 """
-Defining 2 boards, one for the player to have their guesses on and one with 
-the correct solution on it. ANSWER_BOARD is hidden and used to check locations against only 
+Defining 2 boards, one for the player to have their guesses on and one with
+the correct solution on it. ANSWER_BOARD is hidden and used to check locations
+against only.
 """
-
 
 ANSWER_BOARD = [[' '] * 9 for x in range(9)]
 PLAYER_BOARD = [[' '] * 9 for x in range(9)]
 
-"""
-Converting to letters to use at the top of the board for easier overview of 
-the gameboard.
-"""
-
-
-letters_convert = {
-    'A': 0, 
-    'B': 1, 
-    'C': 2, 
-    'D': 3, 
-    'E': 4, 
-    'F': 5, 
-    'G': 6,
-    'H': 7, 
-    'I': 8
-}
-
-
-#Loop to build the boards for both the PLAYER_BOARD and the ANSWER_BOARD.
-
-
+# Loop to build the boards for both the PLAYER_BOARD and the ANSWER_BOARD.
 
 def print_board(board):
-    print('   A B C D E F G H I')
-    print('   +-+-+-+-+-+-+-+-+')
+    print('  A B C D E F G H I')
+    print('  +-+-+-+-+-+-+-+-+')
     row_number = 1
     for row in board:
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
+"""
+Converting to letters to use at the top of the board for easier overview of
+the gameboard.
+"""
 
-#Loop to Put out the 5 battleships that we are gonna try and find.
+letters_convert = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7,
+    'I': 8
+}
+# Loop to Put out the 5 battleships that we are gonna try and find.
 
 
 def create_ships(board):
     for ship in range(5):
-      ship_row, ship_column = randint(0, 8), randint(0, 8)
-      while board[ship_row][ship_column] == 'X':
-        ship_row, ship_column = randint(0, 8), randint(0, 8)
-    board[ship_row][ship_column] = 'X'
-  
-"""
-Function to add the input data on where to drop the bomb. built with loops to 
-make sure correct data is entered. .upper is used to make sure that it will always be
-an uppercase letter that is provided for the code to run.
-"""
+        ship_row, ship_column = randint(0,8), randint(0,8)
+        while board[ship_row][ship_column] == 'X':
+            ship_row, ship_column = get_ship_location()
+        board[ship_row][ship_column] = 'X'
 
+"""
+Function to add the input data on where to drop the bomb. built with loops to
+make sure correct data is entered. .upper is used to make sure that it will
+always be an uppercase letter that is provided for the code to run.
+"""
 
 def get_ship_location():
     row = input('Please enter a ship row 1-9: ')
-    while row not in '12345678':
+    while row not in '123456789':
         print('Please enter a valid row')
         row = input('Please enter a ship row 1-9: ')
     column = input('Please enter a ship column A-I: ').upper()
@@ -79,28 +70,32 @@ def get_ship_location():
     return int(row) - 1, letters_convert[column]
 
 
-#Counts the hits and tells us how many hits we have. 
-
-
+# Counts the hits and tells us how many hits we have.
 
 def count_hit_ships(board):
     count = 0
     for row in board:
-        for colums in row:
+        for column in row:
             if column == "X":
                 count += 1
     return count
 
 create_ships(ANSWER_BOARD)
-
 turns = 25
 
-#function to check if the input data is a hit or a miss, also checks if your winning. 25 turns to find the ships or you lose.
+# if statement only activates when you start the game to print out short rules
 
+if turns == 25:
+    print('LETS PLAY BATTLESHIPS')
+    print('Find the enemies on the map in 25 turns by choosing locations.')
+    print('5 ships are hiding on the map, all must be found to win the game')
 
+""""
+function to check if the input data is a hit or a miss, also checks
+if your winning. 25 turns to find the ships or you lose.
+"""
 
 while turns > 0:
-    print('LETS PLAY BATTLESHIPS')
     print_board(PLAYER_BOARD)
     row, column = get_ship_location()
     if PLAYER_BOARD[row][column] == 'X' or PLAYER_BOARD[row][column] == '*':
@@ -120,4 +115,5 @@ while turns > 0:
         break
     if turns == 0:
         print('No more bombs left you got captured')
+        print_board(ANSWER_BOARD)
         break
